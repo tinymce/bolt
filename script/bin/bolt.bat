c5d6d3bbefe1d591505072057cbd613ee5a035c6
Andrew Herron <thespyder@programmer.net>
2015-06-29 22:01:24 +1000

    Removed windows installer files

--- script/bin/bolt.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal enableextensions
-
-set base=%~dp0
-
-for %%i in (node.bat node.exe) do if not "%%~$PATH:i"=="" set node=%%~$PATH:i
-if "%node%"=="" echo error: node.js is not on the system path && exit /b 1
-
-"%node%" "%base%bolt.js" %*
